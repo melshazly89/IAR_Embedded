8f9c05e8cc5e327990fbd9d3adf25099ba2f1554
Mahmoud Elshazly <mahmoud.elshazly@delphi.com>
2018-03-08 14:46:26 +0200

    Racing conditions

---
+++ Lesson20/Debug/bsp.d
@@ -0,0 +1,18 @@
+bsp.o: ../bsp.c ../TM4C123GH6PM.h C:/ccs7/CMSIS/Include/core_cm4.h \
+ C:/ccs7/CMSIS/Include/core_cmInstr.h C:/ccs7/CMSIS/Include/cmsis_gcc.h \
+ C:/ccs7/CMSIS/Include/core_cmFunc.h C:/ccs7/CMSIS/Include/core_cmSimd.h \
+ ../bsp.h
+
+../TM4C123GH6PM.h:
+
+C:/ccs7/CMSIS/Include/core_cm4.h:
+
+C:/ccs7/CMSIS/Include/core_cmInstr.h:
+
+C:/ccs7/CMSIS/Include/cmsis_gcc.h:
+
+C:/ccs7/CMSIS/Include/core_cmFunc.h:
+
+C:/ccs7/CMSIS/Include/core_cmSimd.h:
+
+../bsp.h:
---
+++ Lesson20/Debug/subdir_rules.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: GNU Compiler'
+	"C:/ti/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-gcc.exe" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DPART_TM4C123GH6PM -I"C:/ccs7/Lesson20" -I"C:/ccs7/CMSIS/Include" -I"C:/ti/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include" -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -specs="nosys.specs" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
